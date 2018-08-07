579a6db081c4752ea73eb066be0d2fec6f5ae80b
FarkasTamas93 <farkas.tamas1993@gmail.com>
2018-08-07 20:35:14 +0200

    Initial commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-//        asd
+        //Intellij github project
+        //VCS --> enable version control integration
+        //.ignore intellij addon letoltese majd settings-->plugins-->install plugin from disk -->es a jar filet hozzadni
+        //majd projektra jobb klikk -->new -->.ignore-->es ott gitgonore-->rakkattintasz a javara-es berakja a gitignoret majd hozza kell irni hogy .idea es *.iml
+        //majd kent be lehet kapcsolni a version control panelt-->ott latszodik mi piros
+        //ott jobb klikk add majd -->commit changes vagy commit 1 file
+        //majd letre kell hozni egy repositoryt githubon
+        //utanna VCS-->push es ott setup remote-->ott berakni a github linket https://github.com/vellindra/GithubDemo.git amit letrehoztal
+        //es igy feltolti
     }
 }
---
+++ src/com/company/Test2.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by Farkas Tamás on 2018.08.07..
+ */
+public class Test2 {
+    //Ez a branchba kerul
+}
